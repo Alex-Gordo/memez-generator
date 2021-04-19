435b3f737f6af3cee1db36fb2bf4b1df1b2ca0eb
Alex-Gordo <81168242+Alex-Gordo@users.noreply.github.com>
2021-04-19 17:16:34 +0300

    Add files via upload
    update

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,31 +1,41 @@
 'use strict'
 
 
-function onInit(){
-    console.log('hi world');
+function onInit() {
     //TODO : render all cards!
+    gCanvas = document.querySelector('#my-canvas');
+    gCtx = gCanvas.getContext('2d')
+}
+
+function onChangeLine(text) {
+    drawNewLine(text)
+}
+
+function onSelectMeme(imgId) {
+    var currUrl = imgId
+    drawImg(currUrl)
 }
 
 
-function onEditMeme(){
-    var strHTML = `
-    <div class="modal-header">
-    <div class="title"> <button onclick="onCloseModal()" class="btn-close">&times;</button>
-    </div>
+// function onEditMeme() {
+//     var strHTML = `
+//     <div class="modal-header">
+//     <div class="title"> <button onclick="onCloseModal()" class="btn-close">&times;</button>
+//     </div>
 
-    </div>
-    </div>`
-    var elModal = document.querySelector('.modal')
-    var elOverlay = document.querySelector('.overlay')
-    elModal.innerHTML = strHTML
-    elModal.style.display = 'flex'
-    elOverlay.style.display = 'flex'
-}
+//     </div>
+//     </div>`
+//     var elModal = document.querySelector('.modal')
+//     var elOverlay = document.querySelector('.overlay')
+//     elModal.innerHTML = strHTML
+//     elModal.style.display = 'flex'
+//     elOverlay.style.display = 'flex'
+// }
 
 
-function onCloseModal() {
-    var elModal = document.querySelector('.modal')
-    var elOverlay = document.querySelector('.overlay')
-    elModal.style.display = 'none'
-    elOverlay.style.display = 'none'
-}
+// function onCloseModal() {
+//     var elModal = document.querySelector('.modal')
+//     var elOverlay = document.querySelector('.overlay')
+//     elModal.style.display = 'none'
+//     elOverlay.style.display = 'none'
+// }
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,22 +1,61 @@
 'use strict'
 
+var gCanvas
+var gCtx
 var gKeywords = { 'happy': 12, 'funny': 1 }
 
 var gImgs = [
-    {
-        id: 1, url: 'img/popo.jpg', keywords: ['happy']
-    }
-];
+    { id: 1, url: '../meme-img/1.jpg', keywords: ['trump'] },
+    { id: 2, url: '../meme-img/2.jpg', keywords: ['dog'] }
+]
 
 var gMeme = {
     selectedImgId: 5,
     selectedLineIdx: 0,
     lines: [
         {
-            txt: 'I never eat Falafel',
+            txt: 'Your text here',
             size: 20,
-            align: 'left',
-            color: 'red'
+            align: 'center',
+            color: 'white'
         }
     ]
 }
+
+var gCurrLine = gMeme.lines[gMeme.selectedLineIdx]
+var gCurrUrl
+
+
+
+function drawImg(url) {
+    gCurrUrl = url // controls the selected image
+    var img = new Image()
+    img.src = gCurrUrl;
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+        drawText(gCurrLine.txt, 250, 50)
+    }
+}
+
+function drawText(text, x, y) {
+    gCtx.lineWidth = 1
+    gCtx.strokeStyle = 'black'
+    gCtx.fillStyle = gCurrLine.color
+    gCtx.font = '25px Impact'
+    gCtx.textAlign = gCurrLine.align
+    gCtx.fillText(text, x, y)
+    gCtx.strokeText(text, x, y)
+}
+
+function drawNewLine(text) {
+    gCurrLine.txt = text
+    drawImg(gCurrUrl)
+}
+
+function getImgById(id) {
+    return gImgs.find(function (img) {
+        return img.id === id
+    })
+}
+
+
